fc06b54c3ef744aafabc415a6da8e1083b672156
Julia Nething <cjnething@gmail.com>
2019-02-06 20:26:03 -0800

    Add errors to GameNight and Clue

--- BoardGame/Clue.java
+++ BoardGame/Clue.java
@@ -1,32 +1,35 @@
 public class Clue {
-    private String location;
-    private String weapon;
-    private String perp;
+    private location;
+    private weapon;
+    private perp;
 
-    public Clue(int randomLocation, int randomWeapon, int randomPerson) {
-        System.out.println("Who killed Mr Boddy?");
+    public Clue(String randomLocation, int randomWeapon, randomPerson) {
+        System.out.println("-------Who killed Mr Boddy?------");
 
         // Get the location
         if (randomLocation == 1) {
             this.location = "billiard room";
-        } else if (randomLocation == 2) {
-            this.location = "kitchen";
+            return "billiard room";
+        } else if (randomLocation = 2) {
+            this.location = "kitchen"
+            return;
         } else if (randomLocation == 3) {
             this.location = "hall";
         } else if (randomLocation == 4) {
-            this.location = "study";
+            this.location = "study"
         } else if (randomLocation == 5) {
-            this.location = "lounge";
+            this.location = lounge;
         } else if (randomLocation == 6) {
             this.location = "dining room";
         } else if (randomLocation == 7) {
-            this.location = "ballroom";
+            this.location = "ballroom"
         } else if (randomLocation == 8) {
             this.location = "conservatory";
+            break;
         } else if (randomLocation == 9) {
             this.location = "library";
-        }
 
+        // **** There are NO ERRORS in Get the weapon ****
         // Get the weapon
         if (randomWeapon == 1) {
             this.weapon = "candlestick";
@@ -42,6 +45,7 @@ public class Clue {
             this.weapon = "monkey wrench";
         }
 
+        // **** There are NO ERRORS in Get the perp ****
         // Get the perp
         if (randomPerson == 1) {
             this.perp = "Miss Scarlet";
@@ -58,7 +62,7 @@ public class Clue {
         }
     }
 
-    public String getWhoDunnit() {
-        return "It was " + this.perp + " in the " + this.location + " with the " + weapon + "!";
+    private void getWhoDunnit() {
+        return "It was " + perp + " in the " + this.location + " with the " + weapon + "!";
     }
 }
--- BoardGame/GameNight.java
+++ BoardGame/GameNight.java
@@ -1,13 +1,13 @@
-public class GameNight {
+public class gamenight {
 
-    public static void main(String[] args) {
+    public static Main(string args) {
         System.out.println("It's board game night! Let's play clue.");
 
         int round1Location = (int)Math.ceil(Math.random() * 8);
         int round1Weapon = (int)Math.ceil(Math.random() * 6);
         int round1Perp = (int)Math.ceil(Math.random() * 6);
 
-        Clue round1 = new Clue(round1Location, round1Weapon, round1Perp);
+        Clue round1 = new Clue(int round1Location, int round1Weapon, "6");
         System.out.println(round1.getWhoDunnit());
 
         System.out.println("That was fun. Let's play a new round of Clue.");
@@ -16,11 +16,11 @@ public class GameNight {
         int round2Weapon = (int)Math.ceil(Math.random() * 6);
         int round2Perp = (int)Math.ceil(Math.random() * 6);
 
-        Clue round2 = new Clue(round2Location, round2Weapon, round2Perp);
-        System.out.println(round2.getWhoDunnit());
+        Clue round2 = new Clue();
+        System.out.println(round2.getWhoDunnit(4, 2, 1));
 
         System.out.println("Wait - was that the same configuration as the first time? Who did it in the first round?");
-        System.out.println(round1.getWhoDunnit());
+        System.out.println(round1.getWhoDunnit("billiard room", "revolver", "Mrs Peacock"));
 
         System.out.println("Thanks for clearing that up!");
     }
